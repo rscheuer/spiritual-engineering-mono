3bca1b9232170df5757940f9865a8002ad60f929
Ryan Scheuer <ryan.scheuer1@gmail.com>
2020-08-09 18:39:06 -0400

    If statement to check for data in url

--- js/main.js
+++ js/main.js
@@ -20,57 +20,63 @@ function getUrlVars() {
 
 var issue = getUrlVars()["issue"];
 console.log(issue);
+	if (issue != null){
+		//alert(issue);
+		// INDIVIDUAL ARTICLES
+		
+		var client = new HttpClient();
+		client.get('https://api.are.na/v2/channels/'+issue+'contents', function(response) {
+			console.log(response);
+			var obj = JSON.parse(response);
+			
+			for(var i = 0; i < obj.contents.length; i++)
+			{
+			    var blocks = obj.contents[i];
+				console.log(obj.contents[i].id)
+				var num = i;
+				var title = obj.contents[i].title;
+				var dsc = obj.contents[i].description;
+				var url = obj.contents[i].source.url;
+				insertData(num, title, url, dsc);
+			}
+			
+			var issueTitle=document.getElementById('issueTitle');
+			var issueClean = issue.replace(/[^a-zA-Z0-9 ]/g, "");
+			issueTitle.innerHTML=issueClean.toUpperCase();
+		
+		});
+		
+		
 
-//alert(issue);
-// INDIVIDUAL ARTICLES
-
-var client = new HttpClient();
-client.get('https://api.are.na/v2/channels/'+issue+'contents', function(response) {
-	console.log(response);
-	var obj = JSON.parse(response);
-	
-	for(var i = 0; i < obj.contents.length; i++)
-	{
-	    var blocks = obj.contents[i];
-		console.log(obj.contents[i].id)
-		var num = i;
-		var title = obj.contents[i].title;
-		var dsc = obj.contents[i].description;
-		var url = obj.contents[i].source.url;
-		insertData(num, title, url, dsc);
 	}
-	
-	var issueTitle=document.getElementById('issueTitle');
-	var issueClean = issue.replace(/[^a-zA-Z0-9 ]/g, "");
-	issueTitle.innerHTML=issueClean.toUpperCase();
+	else{
+		// ISSUES
 
-});
+		var issues = new HttpClient();
+		issues.get('https://api.are.na/v2/channels/spiritualengineering_main/contents', function(response) {
+		//	console.log(response);
+			var obj = JSON.parse(response);
+		//	alert(obj.contents.length)
+			
+			for(var i = 0; i < obj.contents.length; i++)
+			{
+				console.log(obj.contents[i].id)
+				var num = i+1;
+				var title = obj.contents[i].title.toLowerCase();
+				var dsc = obj.contents[i].metadata.description;
+				var date = obj.contents[i].added_to_at;
+				insertIssue(num, title, dsc, date);
+			}
+		
+		});
 
+	}
+
+       
 function insertData(num, title, url, description) {
-  var newRow=document.getElementById('blockTable').getElementsByTagName('tbody')[0].insertRow();
-  newRow.innerHTML="<td>"+num+"</td><td><a target='_blank' href='"+url+"'>"+title+"</a></td><td>"+description+"</td>";
-}
-       
-       
-// ISSUES
-
-var issues = new HttpClient();
-issues.get('https://api.are.na/v2/channels/spiritualengineering_main/contents', function(response) {
-//	console.log(response);
-	var obj = JSON.parse(response);
-//	alert(obj.contents.length)
-	
-	for(var i = 0; i < obj.contents.length; i++)
-	{
-		console.log(obj.contents[i].id)
-		var num = i+1;
-		var title = obj.contents[i].title.toLowerCase();
-		var dsc = obj.contents[i].metadata.description;
-		var date = obj.contents[i].added_to_at;
-		insertIssue(num, title, dsc, date);
-	}
-
-});
+	  var newRow=document.getElementById('blockTable').getElementsByTagName('tbody')[0].insertRow();
+	  newRow.innerHTML="<td>"+num+"</td><td><a target='_blank' href='"+url+"'>"+title+"</a></td><td>"+description+"</td>";
+	}     
 
 
 function insertIssue(num, title, description, date) {
